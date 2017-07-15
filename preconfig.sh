e9e9b6e258bc94b58b0062eadd4a7bf04bf91c5f
arobase13 <arobase13@pkmia.fr>
2017-07-15 17:06:15 +0200

    Update preconfig.sh

--- preconfig.sh
+++ preconfig.sh
@@ -2,3 +2,9 @@
 apt-get install openvpn apache2 php5-mysql mysql-server php5 nodejs unzip git wget sed npm curl
 npm install -g bower
 ln -s /usr/bin/nodejs /usr/bin/node
+git clone https://github.com/Chocobozzz/OpenVPN-Admin openvpn-admin
+cd openvpn-admin
+chown +x ./install.sh
+./install.sh /var/www/html www-data www-data
+apt-get install iptables-persistent
+systemctl start openvpn@server
