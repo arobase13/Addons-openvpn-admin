a7235d8f9c8d32e13c24b448176aa8655bbab072
arobase13 <arobase13@pkmia.fr>
2017-07-15 17:54:33 +0200

    Update preconfig.sh

--- preconfig.sh
+++ preconfig.sh
@@ -4,7 +4,6 @@ npm install -g bower
 ln -s /usr/bin/nodejs /usr/bin/node
 git clone https://github.com/Chocobozzz/OpenVPN-Admin openvpn-admin
 cd openvpn-admin
-chown +x ./install.sh
 ./install.sh /var/www/html www-data www-data
 apt-get install iptables-persistent
 systemctl start openvpn@server
